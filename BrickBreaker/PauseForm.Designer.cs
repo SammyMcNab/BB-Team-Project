42a2c31da25acbc00c9ae0c89bb3d12c9b236d8a
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-10-23 11:18:39 -0400

    Arrow for pause

--- BrickBreaker/PauseForm.Designer.cs
+++ BrickBreaker/PauseForm.Designer.cs
@@ -32,7 +32,9 @@
             this.continueButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.arrow = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).BeginInit();
             this.SuspendLayout();
             // 
             // continueButton
@@ -45,14 +47,15 @@
             this.continueButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.continueButton.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.continueButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(63)))), ((int)(((byte)(33)))));
-            this.continueButton.Location = new System.Drawing.Point(540, 357);
-            this.continueButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.continueButton.Location = new System.Drawing.Point(405, 290);
+            this.continueButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.continueButton.Name = "continueButton";
-            this.continueButton.Size = new System.Drawing.Size(220, 73);
-            this.continueButton.TabIndex = 0;
+            this.continueButton.Size = new System.Drawing.Size(165, 59);
+            this.continueButton.TabIndex = 2;
             this.continueButton.Text = "CONTINUE";
             this.continueButton.UseVisualStyleBackColor = false;
             this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            this.continueButton.Enter += new System.EventHandler(this.continueButton_Enter);
             // 
             // exitButton
             // 
@@ -64,43 +67,56 @@
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.exitButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(63)))), ((int)(((byte)(33)))));
-            this.exitButton.Location = new System.Drawing.Point(51, 357);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exitButton.Location = new System.Drawing.Point(38, 290);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(220, 73);
+            this.exitButton.Size = new System.Drawing.Size(165, 59);
             this.exitButton.TabIndex = 1;
             this.exitButton.Text = "EXIT";
             this.exitButton.UseVisualStyleBackColor = false;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.exitButton.Enter += new System.EventHandler(this.exitButton_Enter);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(53, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(40, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(720, 230);
+            this.pictureBox1.Size = new System.Drawing.Size(540, 187);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // arrow
+            // 
+            this.arrow.BackColor = System.Drawing.Color.Transparent;
+            this.arrow.BackgroundImage = global::BrickBreaker.Properties.Resources.arrow;
+            this.arrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.arrow.Location = new System.Drawing.Point(439, 205);
+            this.arrow.Name = "arrow";
+            this.arrow.Size = new System.Drawing.Size(100, 80);
+            this.arrow.TabIndex = 3;
+            this.arrow.TabStop = false;
+            // 
             // PauseForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::BrickBreaker.Properties.Resources.pauseBackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(850, 525);
+            this.ClientSize = new System.Drawing.Size(638, 427);
+            this.Controls.Add(this.arrow);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.continueButton);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PauseForm";
             this.Text = "PauseForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrow)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,5 +126,6 @@
         private System.Windows.Forms.Button continueButton;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox arrow;
     }
 }
